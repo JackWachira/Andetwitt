b17a3ae47016698a87c4cadd23cb49ee10ee4b97
Jack Mwangi <jack.wachira@andela.com>
2016-08-25 14:47:12 +0300

    (Feature) Tweet model

--- andetwitt/andetwitt/settings.py
+++ andetwitt/andetwitt/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_nose'
+    'django_nose',
+    'tweets'
 ]
 
 MIDDLEWARE = [
